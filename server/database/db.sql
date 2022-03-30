29f1cca4f1cc970c9f5792289baf0dbf28f24230
Marcos Aguado <marcosolda@outlook.es>
2022-03-30 10:16:21 -0300

    Diseño de la Base de Datos.

---
+++ server/database/db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE alkemy_fullstack;
+
+USE alkemy_fullstack;
+
+CREATE TABLE user(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(200) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  is_deleted VARCHAR(1) NOT NULL,
+);
+
+CREATE TABLE transaction(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  concept VARCHAR(50) NOT NULL,
+  amount DOUBLE(9,2) NOT NULL,
+  date VARCHAR(10) NOT NULL,
+  user_id INT NOT NULL,
+  id_type_transaction INT NOT NULL,
+  category_id INT NOT NULL,
+  is_deleted VARCHAR(1) NOT NULL,
+);
+
+CREATE TABLE category(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL,
+  is_deleted VARCHAR(1) NOT NULL,
+);
+
+CREATE TABLE type_transaction(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  type VARCHAR(20) NOT NULL,
+  is_deleted VARCHAR(1) NOT NULL,
+);
