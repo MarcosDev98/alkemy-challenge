f239e8d1f5c619408f5d6e1d56904c15ef273cd8
Marcos Aguado <marcosolda@outlook.es>
2022-03-31 10:50:21 -0300

    Moviendo archivos que los cree en rutas equivocadas.

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const useForm = (initialState) => {
+
+  const [form, setForm] = useState(initialState);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const reset = () => {
+    setForm(initialState);
+  };
+
+  return [form, handleChange, reset];
+
+};
+
+export default useForm;
