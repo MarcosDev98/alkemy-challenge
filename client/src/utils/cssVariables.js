5fa58ead9ebdcf76ea9e663618cfd1d1b8454723
Marcos Aguado <marcosolda@outlook.es>
2022-03-31 20:03:40 -0300

    Se creo archivo para definir variables css.

---
+++ client/src/utils/cssVariables.js
@@ -0,0 +1,22 @@
+export const COLORS = {
+  main_blue: '#0099ff',
+  button_bg: '#2185d0',
+  button_hover: '#2150d0',
+  container_bg: '#cecece',
+  card_bg: '#fff',
+  input_border: 'rgba(34, 34, 34, 0.15)',
+  input_outline: 'rgba(0, 132, 255 , 0.733)',
+  list_balance: '#64b6f5',
+  table_expenses: '#F58282',
+  table_incomes: '#7CF062',
+  login_bg: '#0099ff',
+};
+
+
+export const FONTSIZES = {
+  button: '12px',
+  input: '14px',
+  list_items: '14px',
+  container: '14px',
+  login_title: '27px',
+};
