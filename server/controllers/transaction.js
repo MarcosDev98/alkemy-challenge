dc413f0cf7986f4ceee83b72f6b27fede227c3d1
Marcos Aguado <marcosolda@outlook.es>
2022-04-03 17:53:20 -0300

    Creando controllers. Transaction listo.

---
+++ server/controllers/transaction.js
@@ -0,0 +1,75 @@
+const transactionRouter = require('express').Router();
+const mysqlConnection = require('../connection.js');
+const { is_deleted, is_not_deleted } = require('../utils/globals.js');
+const userExtractor = require('../middlewares/userExtractor');
+
+// deberia tener las consultas en otro lado e importarlas??
+
+transactionRouter.get('/', userExtractor, async (req, res) => {
+
+  const { user_id } = req;
+
+  await mysqlConnection.query(`SELECT id, concept, amount, date, user_id, id_type_transaction, is_deleted, category_id FROM transaction WHERE is_deleted='${is_not_deleted}' AND user_id=${user_id};`, (err, results ) => {
+    if (err) {
+      const error = { code: err.code, message: err.message };
+      response.status(500).json(error);
+      throw error;
+    } else {
+      response.send(results);
+    }
+  });
+
+});
+
+
+transactionRouter.post('/create', userExtractor, async (req, res) => {
+
+  const { concept, amount, date, id_type_transaction } = req.body;
+
+  const { user_id } = req;
+
+  await mysqlConnection.query(`INSERT INTO transaction (concept, amount, date, user_id, id_type_transaction, is_deleted, category_id) VALUES('${concept}', ${amount}, '${date}', ${user_id}, ${id_type_transaction}, '${is_not_deleted}', '${0}');`, (err) => {
+    if (err) {
+      const error = { code: err.code, message: err.message };
+      throw error;
+    } else {
+      response.json({ status: 'OK' });
+    }
+  });
+
+});
+
+transactionRouter.put('/update', userExtractor, async (req, resp) => {
+
+  const { id, concept, amount, date, category_id } = req.body;
+
+  const { user_id } = req;
+
+  await mysqlConnection.query(`UPDATE transaction SET concept='${concept}', amount=${amount}, date='${date}', category_id=${category_id} WHERE id=${id} AND user_id=${user_id};`, (err) => {
+    if (err) {
+      const error = { code: err.code, message: err.message };
+      throw error;
+    } else {
+      response.json({ status: 'OK' });
+    }
+  });
+
+});
+
+transactionsRouter.delete('/delete', userExtractor, async (request, response) => {
+  const { id } = request.body;
+
+  const { user_id } = request;
+
+  await mysqlConnection.query(`UPDATE transaction SET is_deleted='${is_deleted}' WHERE id=${id} AND user_id=${user_id};`, (err) => {
+    if (err) {
+      const error = { code: err.code, message: err.message };
+      throw error;
+    } else {
+      response.json({ status: 'OK' });
+    }
+  });
+
+});
+
+module.exports = transactionsRouter;
