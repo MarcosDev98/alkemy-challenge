b981651bc29454ca226868411c4d4325d96e9884
Marcos Aguado <marcosolda@outlook.es>
2022-04-04 11:09:47 -0300

    Reescribi los imports porque habia error y no lo encontraba.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from './Home';
-export { default as Login } from './Login';
 export { default as Input } from './Input';
+export { default as Login } from './Login';
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  , document.getElementById('root')
-);
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<BrowserRouter><App /></BrowserRouter>);
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+//   , document.getElementById('root')
+// );
