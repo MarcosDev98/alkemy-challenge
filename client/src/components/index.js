4406ec44c273e24bb960a524792686104f5d4df1
Marcos Aguado <marcosolda@outlook.es>
2022-04-05 10:05:28 -0300

    Se agregaron los nuevos componentes al index de /components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,8 @@
+export { default as EditableRow } from './EditableRow';
+export { default as FormTransaction } from './FormTransaction';
 export { default as Home } from './Home';
 export { default as Input } from './Input';
 export { default as Login } from './Login';
+export { default as ReadOnlyRow } from './ReadOnlyRow';
+export { default as Select } from './Select';
+export { default as Table } from './Table';
