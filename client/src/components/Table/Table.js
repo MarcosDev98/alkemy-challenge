dc046d185788db82bfddffed3cc6a49c180ea730
Marcos Aguado <marcosolda@outlook.es>
2022-04-05 10:09:44 -0300

    Componente Table donde se mostraran las transacciones que haya.

---
+++ client/src/components/Table/Table.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from 'react';
+import { ReadOnlyRow, EditableRow, FormTransaction } from '../';
+import { ConceptTH, TableContainer, StyledTable, TableBody, TableHead, TR, TH } from './styles';
+
+import { updateTransaction, deleteTransaction, getTransactions } from '../../services/transaction';
+
+
+const Table = () => {
+
+  const [transactions, setTransactions] = useState([]);
+  const [user, setUser] = useState(null);
+  const [editRow, setEditRow] = useState(null);
+  const [editFormData, setEditFormData] = useState({
+    id: '',
+    concept: '',
+    amount: '',
+    date: '',
+    category_id: ''
+  });
+
+
+  useEffect(() => {
+    const loggedUser = JSON.parse(window.sessionStorage.getItem('loggedUser'));
+    if (loggedUser !== null) {
+
+      setUser(loggedUser);
+    }
+
+  },[]);
+
+
+  useEffect(() => {
+    if (user) {
+      getAllTransactions();
+    }
+  }, [user]);
+
+  const getAllTransactions = () => {
+    getTransactions(user.token)
+      .then((data) => {
+        setTransactions(data);
+      })
+      .catch((error) => {
+        console.error('get_transactions_error', error);
+      });
+  };
+
+  // REEMPLAZAR CON MI HOOK.
+  const handleEditFormChange = (e) => {
+    e.preventDefault();
+
+    const fieldName = e.target.getAttribute('name');
+    const fieldValue = e.target.value;
+
+    const newFormData = { ...editFormData };
+
+    newFormData[fieldName] = fieldValue;
+    
+    setEditFormData(newFormData);
+
+  };
+
+
+  const handleEditFormSubmit = async (e) => {
+    e.preventDefault();
+
+    const editedTransaction = {
+      id: editFormData.id,
+      concept: editFormData.concept,
+      amount: editFormData.amount,
+      date: editFormData.date,
+      category_id: editFormData.category_id,
+    };
+
+
+    updateTransaction(editedTransaction, user.token)
+      .then(() => {
+        console.log('transaction_updated');
+        setEditRow(null);
+        
+        getAllTransactions();
+
+      })
+      .catch((error) => console.error('update_transaction_error', error));
+  };
+
+  const handleDelete = async (event, transaction) => {
+    event.preventDefault();
+
+    const message = '¿Desea eliminar el registro ' + ' ' + transaction.concept + ' ' + transaction.amount + ' ' + transaction.date + '?';
+
+    if (!confirm(message)){
+      return;
+    }
+
+    try {
+      deleteTransaction(transaction.id, user.token)
+        .then(() => {
+          console.log('transaction_deleted');
+          getAllTransactions();
+        })
+        .catch((error) => console.error('delete_transaction_error', error));
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
+
+  const handleCancelClick = () => {
+    setEditRow(null);
+  };
+
+  const handleEdit = (e, transaction) => {
+    e.preventDefault();
+    setEditRow(transaction.id);
+
+    const formValues = {
+      id: transaction.id,
+      concept: transaction.concept,
+      amount: transaction.amount,
+      date: transaction.date,
+      category_id: transaction.category_id,
+    };
+
+    setEditFormData(formValues);
+
+  };
+
+
+  return(
+    <>
+      <TableContainer>
+        <form onSubmit={handleEditFormSubmit}>
+          <StyledTable>
+
+            <TableHead>
+              <TR>
+                <ConceptTH >Concepto</ConceptTH>
+                <TH >Monto</TH>
+                <TH >Fecha</TH>
+                <TH >Categoria</TH>
+                <TH >Acciones</TH>
+              </TR>
+            </TableHead>
+
+
+
+            <TableBody>
+              {transactions.map((x) => {
+                return editRow === x.id ? (
+                  <EditableRow
+                    key={x.id}
+                    editFormData={editFormData}
+                    handleEditFormChange={handleEditFormChange}
+                    transaction={x}
+                    handleCancelClick={handleCancelClick}
+                  />
+                ) : (
+                  <ReadOnlyRow 
+                    key={x.id} 
+                    transaction={x} 
+                    handleEdit={handleEdit} 
+                    handleDelete={handleDelete}
+                  />
+                );
+                
+              
+              })}
+            </TableBody>
+
+          </StyledTable>
+        </form>
+      </TableContainer>
+      <FormTransaction getTransactions={getAllTransactions} />
+    </>
+  );
+
+};
+
+export default Table;
---
+++ client/src/components/Table/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { COLORS } from '../../utils/cssVariables';
+
+const { table_incomes, table_expenses } = COLORS;
+
+
+export const TableContainer = styled.div`
+  
+  color: #0B1827;
+  margin: 0 auto;
+  
+`;
+
+
+
+export const StyledTable = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+
+`;
+
+export const TableHead = styled.thead``;
+
+export const TableBody = styled.tbody``;
+
+export const TR = styled.tr`
+
+  background-color: ${props => props.bg === 'green' ? table_incomes : table_expenses };
+  width: 100%;
+
+`;
+
+export const TH = styled.th`
+  text-align: center;
+  font-weight: 600;
+  padding: 6px 12px;
+  background-color: #494949;
+  /* color: #00CFFD; */
+  color: #fff;
+`;
+
+export const TD = styled.td`
+  width: 25%;
+  padding: 6px 12px;
+`;
+
+export const ConceptTH = styled(TH)`
+  text-align: left;
+`;
