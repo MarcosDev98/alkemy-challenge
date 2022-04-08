59a506f4bd5f4020060da5f1f65f26d47d9103bd
Marcos Aguado <marcosolda@outlook.es>
2022-04-08 12:13:19 -0300

    Se modifico, pero falta agregar proteccion de rutas.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Home, Login } from './components';
+import { Home, Login, Navbar } from './components';
 
 const App = () => {
 
+  const [user, setUser] = useState(null);
+
+  const loggedUser = JSON.parse(window.sessionStorage.getItem('loggedUser'));
+
+  useEffect(() => {
+
+    if (loggedUser !== null) {
+
+      setUser(loggedUser);
+    }
+
+  },[]);
+
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/' element={() => {
+          user ? <Home /> : <Login />;}
+        } />
       </Routes>
     </>
   );
