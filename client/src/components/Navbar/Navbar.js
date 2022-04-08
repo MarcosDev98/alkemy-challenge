e64ea39fe6723023b81418d7218861a65bd14aab
Marcos Aguado <marcosolda@outlook.es>
2022-04-08 12:11:38 -0300

    Componente Navbar para navegacion.

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Nav } from './styles';
+
+const Navbar = () => {
+
+  return (
+    <Nav>
+      <p>Categorias</p>
+      <p>Home</p>
+      <p>Login</p>
+    </Nav>
+  );
+
+};
+
+export default Navbar;
